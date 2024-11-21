ef51994d3383d42a5910e05e081b9970512829c8
Iman Andrea <92734842+AerdnaNami@users.noreply.github.com>
2024-11-21 19:05:32 +0400

    Add files via upload

--- lstm_code/multitask_script_arg_BERT.py
+++ lstm_code/multitask_script_arg_BERT.py
@@ -28,7 +28,7 @@ snow = nltk.stem.SnowballStemmer('english')
 
 warnings.filterwarnings("ignore")                    
 
-train_data = 'dataset/asap++_data.csv'
+train_data = '/home/iman.alsikaiti/workspace/AI_project/dataset/asap++_data.csv'
 df = pd.read_csv(train_data)
 
 feature_1 = 'word_choice'
@@ -52,7 +52,7 @@ data_lang = []
 data_prompt = []
 data_nar = []
 
-num = [1,2,3,4,5,6]
+num = [0,1,2,3,4,5,6]
 count = 0
 
 def gen_num(num,length):
@@ -60,6 +60,7 @@ def gen_num(num,length):
     return num_list
 
 def get_scores(feature):
+    list_0 = []
     list_1 = []
     list_2 = []
     list_3 = []
@@ -68,6 +69,8 @@ def get_scores(feature):
     list_6 = []
     # getting essays that are the scores in num
     for i,j in zip(range(len(df)),df["essay"]):
+        if df[feature][i] == 0: # essays scored 1
+            list_0.append(j)
         if df[feature][i] == 1: # essays scored 1
             list_1.append(j)
         if df[feature][i] == 2:
@@ -81,46 +84,50 @@ def get_scores(feature):
         if df[feature][i] == 6:
             list_6.append(j)
 
-        data = list(itertools.chain(list_1,list_2,list_3,list_4,list_5,list_6))
+        data = list(itertools.chain(list_0,list_1,list_2,list_3,list_4,list_5,list_6))
 
-    return list_1, list_2, list_3, list_4, list_5, list_6, data
+    return list_0,list_1, list_2, list_3, list_4, list_5, list_6, data
 
-list_1, list_2, list_3, list_4, list_5, list_6, data_org = get_scores(feature = feature_1)
+list_0, list_1, list_2, list_3, list_4, list_5, list_6, data_org = get_scores(feature = feature_1)
+score_org_0 = gen_num(0,len(list_0))
 score_org_1 = gen_num(1,len(list_1))
 score_org_2 = gen_num(2,len(list_2))
 score_org_3 = gen_num(3,len(list_3))
 score_org_4 = gen_num(4,len(list_4))
 score_org_5 = gen_num(5,len(list_5))
 score_org_6 = gen_num(6,len(list_6))
-score_org = list(itertools.chain(score_org_1,score_org_2,score_org_3,score_org_4,score_org_5,score_org_6))
+score_org = list(itertools.chain(score_org_0, score_org_1,score_org_2,score_org_3,score_org_4,score_org_5,score_org_6))
 
-list_1, list_2, list_3, list_4, list_5, list_6, data_word = get_scores(feature=feature_2)
+list_0, list_1, list_2, list_3, list_4, list_5, list_6, data_word = get_scores(feature=feature_2)
+score_word_0 = gen_num(0,len(list_0))
 score_word_1 = gen_num(1,len(list_1))
 score_word_2 = gen_num(2,len(list_2))
 score_word_3 = gen_num(3,len(list_3))
 score_word_4 = gen_num(4,len(list_4))
 score_word_5 = gen_num(5,len(list_5))
 score_word_6 = gen_num(6,len(list_6))
-score_word = list(itertools.chain(score_word_1,score_word_2,score_word_3,score_word_4,score_word_5,score_word_6))
+score_word = list(itertools.chain(score_word_0, score_word_1,score_word_2,score_word_3,score_word_4,score_word_5,score_word_6))
 
-list_1, list_2, list_3, list_4, list_5, list_6, data_sent = get_scores(feature=feature_3)
+list_0, list_1, list_2, list_3, list_4, list_5, list_6, data_sent = get_scores(feature=feature_3)
+score_sent_0 = gen_num(0,len(list_0))
 score_sent_1 = gen_num(1,len(list_1))
 score_sent_2 = gen_num(2,len(list_2))
 score_sent_3 = gen_num(3,len(list_3))
 score_sent_4 = gen_num(4,len(list_4))
 score_sent_5 = gen_num(5,len(list_5))
 score_sent_6 = gen_num(6,len(list_6))
-score_sent = list(itertools.chain(score_sent_1,score_sent_2,score_sent_3,score_sent_4,score_sent_5,score_sent_6))
+score_sent = list(itertools.chain(score_sent_0, score_sent_1,score_sent_2,score_sent_3,score_sent_4,score_sent_5,score_sent_6))
 print(len(score_sent), len(data_sent))
 
-list_1, list_2, list_3, list_4, list_5, list_6, data_conv = get_scores(feature=feature_4)
+list_0, list_1, list_2, list_3, list_4, list_5, list_6, data_conv = get_scores(feature=feature_4)
+score_conv_0 = gen_num(0,len(list_0))
 score_conv_1 = gen_num(1,len(list_1))
 score_conv_2 = gen_num(2,len(list_2))
 score_conv_3 = gen_num(3,len(list_3))
 score_conv_4 = gen_num(4,len(list_4))
 score_conv_5 = gen_num(5,len(list_5))
 score_conv_6 = gen_num(6,len(list_6))
-score_conv = list(itertools.chain(score_conv_1,score_conv_2,score_conv_3,score_conv_4,score_conv_5,score_conv_6))
+score_conv = list(itertools.chain(score_conv_0, score_conv_1,score_conv_2,score_conv_3,score_conv_4,score_conv_5,score_conv_6))
 print(len(score_conv), len(data_conv))
 
 # dictionary of lists
@@ -173,19 +180,20 @@ def get_multitask_source():
     x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, dropout=0.5))(x)
     x = Dropout(0.2)(x)
 
-    # Output layers for each task
-    lang_score_output = Dense(1, activation='relu', name='lang_score', kernel_regularizer=l2(0.01))(x)
-    prompt_score_output = Dense(1, activation='relu', name='prompt_score', kernel_regularizer=l2(0.01))(x)
-    nar_score_output = Dense(1, activation='relu', name='nar_score', kernel_regularizer=l2(0.01))(x)
+    # Output layer for each score type
+    organization_score_output = Dense(1, activation='relu', name='org_score', kernel_regularizer=l2(0.01))(x)
+    word_choice_score_output = Dense(1, activation='relu', name='word_score', kernel_regularizer=l2(0.01))(x)
+    sentence_score_output = Dense(1, activation='relu', name='sentence_score', kernel_regularizer=l2(0.01))(x)
+    conventions_score_output = Dense(1, activation='relu', name='conventions_score', kernel_regularizer=l2(0.01))(x)
 
     # Define the model with multiple outputs
-    model = Model(inputs=[input_ids, attention_mask], outputs=[lang_score_output, prompt_score_output, nar_score_output])
+    model = Model(inputs=[input_ids, attention_mask], outputs=[organization_score_output, word_choice_score_output, sentence_score_output, conventions_score_output])
 
     # Compile the model
     model.compile(
-        loss={'lang_score': 'mean_squared_error', 'prompt_score': 'mean_squared_error', 'nar_score': 'mean_squared_error'},
-        optimizer='adam',
-        metrics={'lang_score': 'mae', 'prompt_score': 'mae', 'nar_score': 'mae'}
+        loss={'org_score': 'mean_squared_error', 'word_score': 'mean_squared_error', 'sentence_score': 'mean_squared_error', 'conventions_score': 'mean_squared_error'},
+        optimizer='adam', 
+        metrics={'org_score': 'mae', 'word_score': 'mae', 'sentence_score': 'mae', 'conventions_score': 'mae'}
     )
 
     model.summary()
